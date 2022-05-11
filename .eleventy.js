ad3c01c801f77c6a7e978dc6f3595733a3512746
Henry Desroches <yo@henry.codes>
2022-05-10 23:15:54 -0600

    ⏰ - Add date filter to 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "*.{woff2,jpg,}": "" });
     eleventyConfig.addPassthroughCopy({ "meta": "meta" });
+
+    eleventyConfig.addFilter('jsDate', str => {
+        return new Date(str);
+    })
     return {
         htmlTemplateEngine: 'njk'
     }
